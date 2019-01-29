06176b785dff620c06b7f9951f62ba47faaa2d46
arc13-13 <13.archit@gmail.com>
2019-01-29 23:16:10 +0530

    Initial Commit

---
+++ gitload/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  gitload
+//
+//  Created by Akshat Agrawal on 29/01/19.
+//  Copyright © 2019 Akshat Agrawal. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
